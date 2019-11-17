6e97ed423a64c468c3e395992ac0e67a2fa20d61
Dima Kushch <deamand789@gmail.com>
2019-11-17 13:58:35 +0200

    Add WindowsFormsGame

---
+++ PimpMyRide.WindowsFormsApp/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PimpMyRide.WindowsFormsApp
+{
+    static class Program
+    {
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var pimpMyRideForm = new PimpMyRideForm();
+            var game = new WindowsFormsGame(pimpMyRideForm);
+            game.Run();
+            Application.Run(pimpMyRideForm);
+        }
+    }
+}
---
+++ PimpMyRide.WindowsFormsApp/WindowsFormsGame.cs
@@ -0,0 +1,83 @@
+﻿using PimpMyRide.Domain;
+
+namespace PimpMyRide.WindowsFormsApp
+{
+    public class WindowsFormsGame:Game
+    {
+        private PimpMyRideForm Form { get; }
+
+        public WindowsFormsGame(PimpMyRideForm form)
+        {
+            Form = form;
+        }
+        protected override void ShowState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformGameStarted()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformBalance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformMoneyIsOver()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override int RequestChoice()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformMove()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformCantMove()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformNothingToDo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformGameOver()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformNotReplacedBecauseOfBalance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformReplace()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformUnrepairable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformNotRepairedBecauseOfBalance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InformRepaired()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
