4d7d063ef213654d4397558bd914077e606e1f63
Dima Kushch <deamand789@gmail.com>
2019-11-17 17:37:13 +0200

    add button

--- PimpMyRide.WindowsFormsApp/PimpMyRideForm.Designer.cs
+++ PimpMyRide.WindowsFormsApp/PimpMyRideForm.Designer.cs
@@ -29,22 +29,71 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(165, 40);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(541, 151);
             this.textBox1.TabIndex = 0;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(482, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(597, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // PimpMyRideForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
             this.Name = "PimpMyRideForm";
             this.Text = "PimpMyRideForm";
 
@@ -53,6 +102,10 @@
         #endregion
 
         public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- PimpMyRide.WindowsFormsApp/PimpMyRideForm.cs
+++ PimpMyRide.WindowsFormsApp/PimpMyRideForm.cs
@@ -22,11 +22,9 @@ namespace PimpMyRide.WindowsFormsApp
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-
             if (int.TryParse(textBox1.Text.Split(Environment.NewLine).Last(), out var input))
             {
                 choice = input;
-                
             }
         }
 
@@ -56,6 +54,43 @@ namespace PimpMyRide.WindowsFormsApp
         public void RequestInput()
         {
             Invoke((Action) (() => choice = null));
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            choice = 1;
+            DisableControls();
+        }
+
+
+        private void DisableControls()
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            choice = 2;
+            DisableControls();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            choice = 3;
+            DisableControls();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            choice = 4;
+            DisableControls();
         }
     }
 }
