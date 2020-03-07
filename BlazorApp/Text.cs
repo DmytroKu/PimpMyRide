6fd398d2a60679fcfd3a4e7c90d302599e28dd19
Dima Kushch <deamand789@gmail.com>
2020-03-07 18:19:28 +0200

    Text renderer

---
+++ BlazorApp/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class Text
+    {
+        public List<string> Value { get; private set; }
+
+        public Text()
+        {
+            Value = new List<string>();
+        }
+
+        public async Task Run(Func<Task> refreshUi)
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                Value.Add("line");
+                await refreshUi();
+            }
+        }
+    }
+}
